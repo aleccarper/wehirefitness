0a22553b0547de945b9ef42f2619f479f91dd103
Alec Carper <xarious@gmail.com>
2016-03-23 22:30:10 -0600

    coupon javascript

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,11 +1,12 @@
 class CouponsController < ApplicationController
   respond_to :json
-  
+
   def lookup
     coupon = Coupon.find_by_code(params[:code])
     response = {}
     if coupon
       response[:found] = true
+      response[:code] = coupon.code
       response[:can_use] = coupon.can_use?
       response[:percent_off] = coupon.percent_off
       response[:as_float] = coupon.as_float
